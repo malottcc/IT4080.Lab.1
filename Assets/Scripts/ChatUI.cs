006f90f7ad6d87d3d19593f47482ecdca0731950
malottcc <malottcolin@gmail.com>
2023-02-13 18:00:53 -0500

    First test

---
+++ Assets/Scripts/ChatUI.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChatUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var stupid; 
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
