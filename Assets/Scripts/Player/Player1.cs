6667f6ca9726e900bdb5ab5321908ae016f424ab
malottcc <malottcolin@gmail.com>
2023-02-27 15:03:15 -0500

    Monday Update

--- Assets/Scripts/Player/Player1.cs
+++ Assets/Scripts/Player/Player1.cs
@@ -45,5 +45,53 @@ public class Player1 : NetworkBehaviour
         turn.x += Input.GetAxis("Mouse X");
         turn.y += Input.GetAxis("Mouse Y");
         transform.localRotation = Quaternion.Euler(-turn.y, turn.x, 0);
+
+        /*if (Input.GetButtonDown("Fire1"))
+        {
+            RequestNextColorServerRpc();
+        }*/
+
     }
+
+
+
+    //------
+    //Color
+
+    /*private static Color[] availColors = new Color[] {
+            Color.black, Color.blue, Color.cyan,
+            Color.gray, Color.green, Color.yellow };
+    private int hostColorIndex = 0;
+    public NetworkVariable netPlayerColor = new NetworkVariable;
+
+
+    public override void OnNetworkSpawn()
+    {
+        netPlayerColor.OnValueChanged += OnPlayerColorChanged;
+    }
+
+
+    public void ApplyPlayerColor()
+    {
+        GetComponent().material.color = netPlayerColor.Value;
+    }
+
+
+    public void OnPlayerColorChanged(Color previous, Color current)
+    {
+        ApplyPlayerColor();
+    }
+
+    [ServerRpc]
+    void RequestNextColorServerRpc(ServerRpcParams serverRpcParams = default)
+    {
+        hostColorIndex += 1;
+        if (hostColorIndex > availColors.Length - 1)
+        {
+            hostColorIndex = 0;
+        }
+
+        Debug.Log($"host color index = {hostColorIndex} for {serverRpcParams.Receive.SenderClientId}");
+        netPlayerColor.Value = availColors[hostColorIndex];
+    }*/
 }
